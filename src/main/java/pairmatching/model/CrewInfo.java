38f76fcf20d3656613e1bec18b7e17ba84099e32
ppparkta <pppppark2@gmail.com>
2024-12-13 21:33:44 +0900

    feat(View): Console Input 추가

--- src/main/java/pairmatching/model/CrewInfo.java
+++ src/main/java/pairmatching/model/CrewInfo.java
@@ -7,6 +7,14 @@ public class CrewInfo {
     private final List<Crew> backendCrews = new ArrayList<>();
     private final List<Crew> frontendCrews = new ArrayList<>();
 
+    public List<Crew> getBackendCrews() {
+        return List.copyOf(backendCrews);
+    }
+
+    public List<Crew> getFrontendCrews() {
+        return List.copyOf(frontendCrews);
+    }
+
     public static CrewInfo of(List<Crew> backends, List<Crew> frontends) {
         CrewInfo crewInfo = new CrewInfo();
         crewInfo.backendCrews.addAll(backends);
